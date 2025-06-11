b008711dd78ab032306808aab1a52552101475ef
zaza090977777@gmail.com <zaza090977777@gmail.com>
2025-06-11 14:16:29 +0300

    updates

--- config/socket.js
+++ config/socket.js
@@ -156,7 +156,7 @@ const setupSocket = (server) => {
       socket.to(roomId).emit("user_left", {
         user_id: socket.userId,
         user_type: socket.userType,
-        chat_id: socket.chat_id,
+        chat_id: chat_id,
       });
       socket.leave(roomId);
       console.log(`User ${socket.userId} left chat ${chat_id}`);
